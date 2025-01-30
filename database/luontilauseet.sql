29e4308b2ea47e4d45d068414d88225280a88c7c
Onni Pylvänen <onni.pylvanen@gmail.com>
2025-01-30 17:49:58 +0200

    update luontilauseet

--- database/luontilauseet.sql
+++ database/luontilauseet.sql
@@ -13,3 +13,43 @@ CREATE TABLE Teos ( -- Yksittäinen teos
     teostyyppi_id INT,
     CONSTRAINT fk_teostyyppi FOREIGN KEY (teostyyppi_id) REFERENCES Teostyyppi(teostyyppi_id)
 );
+
+-- Taulu Asiakkaalle
+CREATE TABLE Asiakas (
+    asiakasID INT,
+    nimi VARCHAR(50),
+    osoite  VARCHAR(100),
+    email VARCHAR(50) ,
+    puhelin VARCHAR(13)
+    PRIMARY KEY (asiakasID),
+    FOREIGN KEY (tilausID) REFERENCES Tilaus(tilausID)
+);
+ 
+-- Taulu Tilaus
+CREATE TABLE Tilaus (
+    tilausID INT,
+    kokonaispaino INT,
+    postikulut FLOAT,
+    kokonaishinta FLOAT,
+    PRIMARY KEY (tilausID),
+    FOREIGN KEY (teosID) REFERENCES Teos(teosID)
+);
+
+CREATE TABLE KeskusDivari ( 
+    keskus_id SERIAL PRIMARY KEY, 
+    nimi VARCHAR(255) NOT NULL, 
+    osoite VARCHAR(255) NOT NULL, 
+    web_sivu VARCHAR(255) 
+); 
+ 
+CREATE TABLE Divari ( 
+    divari_id SERIAL PRIMARY KEY, 
+    keskus_id INT, 
+    teos_id INT, 
+    teostyyppi_id INT, 
+    nimi VARCHAR(255) NOT NULL, 
+    osoite VARCHAR(255) NOT NULL, 
+    FOREIGN KEY (keskus_id) REFERENCES KeskusDivari(id), 
+    FOREIGN KEY (teos_id) REFERENCES Teos(id), 
+    FOREIGN KEY (teostyyppi_id) REFERENCES TeosTyyppi(id)  
+); 
