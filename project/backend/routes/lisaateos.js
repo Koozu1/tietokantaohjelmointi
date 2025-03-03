498e05e663ca0298699f514b60313f22bc83a2d1
Nipsu Nevala <nevalaniina@gmail.com>
2025-03-03 14:36:39 +0200

    Teoksen lisäys päivitys tietokantamuutosten jälkeen

--- project/backend/routes/lisaateos.js
+++ project/backend/routes/lisaateos.js
@@ -4,50 +4,23 @@ import { pool } from "../db.js";
 const router = express.Router();
 
 router.post("/lisaateos", async (req, res) => {
-  const { nimi, isbn, hinta, sisäänostohinta, paino, teostyyppi } = req.body;
-  let client;
-
+  const { nimi, tekijä, isbn, julkaisuvuosi, teostyyppi, paino, hinta, sisäänostohinta } = req.body;
   try {
-    console.log("Received request to add teos:", req.body);
-
-    client = await pool.connect();
-
-    // Check if teostyyppi exists
-    let teostyyppiResult = await client.query(
-      'SELECT teostyyppi_id FROM Keskusdivari.Teostyyppi WHERE tyyppi_nimi = $1',
-      [teostyyppi]
+    const teosResult = await pool.query(
+      'INSERT INTO d1_divari.teos (nimi, tekijä, isbn, julkaisuvuosi, teostyyppi, paino, lähde_skeema) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING teos_id',
+      [nimi, tekijä, isbn, julkaisuvuosi, teostyyppi, paino, 'd1']
     );
-    console.log("Teostyyppi query result:", teostyyppiResult.rows);
+    const teos_id = teosResult.rows[0].teos_id;
 
-    // If teostyyppi does not exist, insert a new record into Teostyyppi table
-    let teostyyppi_id;
-    if (teostyyppiResult.rows.length === 0) {
-      teostyyppiResult = await client.query(
-        'INSERT INTO Keskusdivari.Teostyyppi (tyyppi_nimi) VALUES ($1) RETURNING teostyyppi_id',
-        [teostyyppi]
-      );
-      teostyyppi_id = teostyyppiResult.rows[0].teostyyppi_id;
-      console.log("Inserted new teostyyppi:", teostyyppi_id);
-    } else {
-      teostyyppi_id = teostyyppiResult.rows[0].teostyyppi_id;
-      console.log("Found existing teostyyppi:", teostyyppi_id);
-    }
-
-    // Insert into Teos table
-    const result = await client.query(
-      'INSERT INTO Keskusdivari.Teos (nimi, isbn, hinta, sisäänostohinta, paino, teostyyppi_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
-      [nimi, isbn, hinta, sisäänostohinta, paino, teostyyppi_id]
+    const nideResult = await pool.query(
+      'INSERT INTO d1_divari.nide (hinta, sisäänostohinta, lähde_skeema, tila, teos_id) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+      [hinta, sisäänostohinta, 'd1', 'available', teos_id]
     );
-    console.log("Inserted new teos:", result.rows[0]);
 
-    res.status(201).json(result.rows[0]);
+    res.status(201).json({ teos: teosResult.rows[0], nide: nideResult.rows[0] });
   } catch (error) {
-    console.error('Error inserting data:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  } finally {
-    if (client) {
-      client.release();
-    }
+    console.error('Error inserting new book or copy:', error.message);
+    res.status(500).json({ error: error.message });
   }
 });
 
