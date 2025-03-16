a845a3ac2867e35d6648034c06abf18e3644b73e
Noora Ahonen <noora.ahonen@tuni.fi>
2025-03-16 17:07:02 +0200

    Muokattu sivun ulkonäköä ja etusivua

--- project/react-frontend/src/pages/LisaaTeos.jsx
+++ project/react-frontend/src/pages/LisaaTeos.jsx
@@ -56,7 +56,7 @@ const LisaaTeos = () => {
   };
 
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-auto p-4 max-w-[800px]">
       <Link to="/frontpage" className="text-blue-500 hover:underline">Takaisin etusivulle</Link>
       <h1 className="text-2xl font-bold mb-4">Lisää Teos</h1>
       <Notification message={notification.message} type={notification.type} />
@@ -75,7 +75,7 @@ const LisaaTeos = () => {
         </button>
       </div>
       <form onSubmit={handleSubmit} className="space-y-4">
-      {activeTab === "keskusdivari" && (
+        {activeTab === "keskusdivari" && (
           <div>
             <label className="block text-sm font-medium text-gray-700">Divari ID:</label>
             <input
